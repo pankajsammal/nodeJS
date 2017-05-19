10e25fd4065195f7308af9221b6bb4d22ae739c8
Pankaj Sammal <pankajsammal.qg@gmail.com>
2017-05-19 13:28:03 +0530

    chapter5

--- app.js
+++ app.js
@@ -1,39 +1,9 @@
-/**
- * Created by sam29 on 19-May-17.
- */
-// var person = {
-//     firstName :"Pankaj",
-//     lastName : "Sammal"
-// };
-// function addNum(a,b) {
-//     return a+b;
-//
+// var sam = {
+//     favFood:"Pizza",
+//     favMovie:"Marvel"
 // }
-// function worthless() {
-//     //return a+b;
-//
-// }
-// var printData = function () {
-//     console.log("Its the function printing");
-// }
-// console.log(addNum);
-// printData();
-// setTimeout(printData,5000);
-function placeAndOrder(orderNo) {
-    console.log("Customer Order",orderNo);
-    cook(function () {
-        console.log("Dilivered food",orderNo);
-    });
-}
-
-function cook(callback) {
-setTimeout(callback,5000)
-}
-
-placeAndOrder(1);
-placeAndOrder(2);
-placeAndOrder(3);
-placeAndOrder(4);
-placeAndOrder(5);
-placeAndOrder(6);
-
+// var others = sam;
+// others.favFood="Pan Cakes";
+// console.log(sam);
+console.log(19=='19');
+console.log(19==='19');
