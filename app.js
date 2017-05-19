5dca44e45ad410329ed5ae828b80f836f2400ed0
Pankaj Sammal <pankajsammal.qg@gmail.com>
2017-05-19 13:36:40 +0530

    chapter6

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-// var sam = {
-//     favFood:"Pizza",
-//     favMovie:"Marvel"
-// }
-// var others = sam;
-// others.favFood="Pan Cakes";
-// console.log(sam);
-console.log(19=='19');
-console.log(19==='19');
+var sam = {
+    printFirstName: function () {
+    console.log("INSIDE printFirstName");
+        console.log(this);
+    console.log(this===sam);
+}
+};
+sam.printFirstName();
+function somethingWorthless() {
+ console.log("You are worthless");
+    console.log(this===sam);
+    console.log(this===global);
+}
+somethingWorthless();
